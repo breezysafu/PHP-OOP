96eccc88ea1abb92bdf56ec5a3850c30af4d1582
Sarfraj <breezy.safu@gmail.com>
2018-12-12 15:35:30 +0545

    completed

---
+++ LoginandRegistration/login_submit.php
@@ -0,0 +1,13 @@
+<?php
+include "class.php";
+extract($_POST);
+extract($_GET);
+if(isset($login)){
+    $select="select email,password from register where email='$e' and password='$p'";
+    if($obj->login($select)){
+        $_SESSION['email']=$e;
+        $obj->url("home.php?msg=run");
+    }else {
+        $obj->url("index.php?err=error");
+    }
+}
